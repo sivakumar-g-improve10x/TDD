e3b225b0561d2b7383aa28b16801abf9eb62c191
SivaKumar <sivakumar.g@improve10x.com>
2023-07-19 14:46:50 +0530

    I wrote code for negativeInteger test in fibonacci class.Testcase is passed.

---
+++ src/fibonacci/Fibonacci.java
@@ -0,0 +1,11 @@
+package fibonacci;
+
+public class Fibonacci {
+
+    public int findFibonacci(int n) {
+        if (n<0){
+            return -1;
+        }
+        return n;
+    }
+}
--- src/fibonacci/FibonacciTest.java
+++ src/fibonacci/FibonacciTest.java
@@ -1,5 +1,19 @@
 package fibonacci;
 
-public class FibonacciTest {
+import org.junit.jupiter.api.Test;
 
-}
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FibonacciTest {
+    @Test
+    public void nothing(){
+
+    }
+    @Test
+    public void givenAnyNegativeInteger_returnNegativeOne() {
+        Fibonacci fibonacci = new Fibonacci();
+        int negativeFibonacci = fibonacci.findFibonacci(-5);
+        assertEquals(-1,negativeFibonacci);
+        }
+    }
+
