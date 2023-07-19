ad6a365b562fcd113d885274ce0dc4b776928888
SivaKumar <sivakumar.g@improve10x.com>
2023-07-19 17:58:35 +0530

    I wrote B Testcase is passed.

--- src/scoring/Scoring.java
+++ src/scoring/Scoring.java
@@ -5,6 +5,8 @@ public class Scoring {
     public int[] calculateScore(String score) {
         if (score.equals("A")){
             return new int[]{1,0,0};
+        } else if (score.equals("B")) {
+            return new int[]{0,1,0};
         }
         return new int[3];
     }
--- src/scoring/ScoringTest.java
+++ src/scoring/ScoringTest.java
@@ -1,31 +1,39 @@
 package scoring;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScoringTest {
+    private Scoring scoring;
+
+    @BeforeEach
+    public void setup(){
+            scoring = new Scoring();
+    }
     @Test
     public void nothing(){
 
     }
     @Test
     public void givenNull_returnsAllZeros(){
-        Scoring scoring = new Scoring();
        int[] allZeros = scoring.calculateScore(null);
        assertArrayEquals(new int[3],allZeros);
     }
     @Test
     public void givenEmpty_returnsAllZeros(){
-        Scoring scoring = new Scoring();
         int[] allZeros = scoring.calculateScore("");
         assertArrayEquals(new int[3],allZeros);
     }
     @Test
     public void givenA_returnsOneZeroZero(){
-        Scoring scoring = new Scoring();
         int[] onlyAScore = scoring.calculateScore("A");
         assertArrayEquals(new int[]{1,0,0},onlyAScore);
+    }
+    @Test
+    public void givenB_returnsZeroOneZero(){
+        int[] onlyBScore = scoring.calculateScore("B");
+        assertArrayEquals(new int[]{0,1,0},onlyBScore);
     }
 }
