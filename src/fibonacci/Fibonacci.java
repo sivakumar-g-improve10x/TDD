304ce146371de52c3fe53b25c0be7b52aabceca0
SivaKumar <sivakumar.g@improve10x.com>
2023-07-19 15:39:35 +0530

    I Completed Fibonacci series.All Testcase is passed.

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -5,10 +5,8 @@ public class Fibonacci {
     public int findFibonacci(int n) {
         if (n<=0){
             return -1;
-        } else if (n==1) {
-            return 0;
-        } else if (n==2) {
-            return 1;
+        } else if (n==1 || n==2) {
+            return n-1;
         }else {
            return findFibonacci(n-1) + findFibonacci(n-2);
         }
--- src/fibonacci/FibonacciTest.java
+++ src/fibonacci/FibonacciTest.java
@@ -1,53 +1,53 @@
 package fibonacci;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FibonacciTest {
+    Fibonacci fibonacci;
+
+    @BeforeEach
+    public void setup(){
+        fibonacci = new Fibonacci();
+    }
     @Test
     public void nothing(){
 
     }
     @Test
     public void givenAnyNegativeInteger_returnNegativeOne() {
-        Fibonacci fibonacci = new Fibonacci();
         int negativeFibonacci = fibonacci.findFibonacci(-15);
         assertEquals(-1,negativeFibonacci);
         }
     @Test
     public void givenZero_returnNegativeOne() {
-        Fibonacci fibonacci = new Fibonacci();
         int negativeFibonacci = fibonacci.findFibonacci(0);
         assertEquals(-1,negativeFibonacci);
     }
     @Test
     public void givenOne_returnZero() {
-        Fibonacci fibonacci = new Fibonacci();
         int firstFibonacci = fibonacci.findFibonacci(1);
         assertEquals(0,firstFibonacci);
     }
     @Test
     public void givenTwo_returnOne() {
-        Fibonacci fibonacci = new Fibonacci();
         int secondFibonacci = fibonacci.findFibonacci(2);
         assertEquals(1,secondFibonacci);
     }
     @Test
     public void givenThree_returnOne() {
-        Fibonacci fibonacci = new Fibonacci();
         int thirdFibonacci = fibonacci.findFibonacci(3);
         assertEquals(1,thirdFibonacci);
     }
     @Test
     public void givenFour_returnTwo() {
-        Fibonacci fibonacci = new Fibonacci();
         int thirdFibonacci = fibonacci.findFibonacci(4);
         assertEquals(2,thirdFibonacci);
     }
     @Test
     public void givenFive_returnTwo() {
-        Fibonacci fibonacci = new Fibonacci();
         int thirdFibonacci = fibonacci.findFibonacci(5);
         assertEquals(3,thirdFibonacci);
     }
