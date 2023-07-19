7d4b8b822cab86bc7f64a102b65b615a18040d9c
SivaKumar <sivakumar.g@improve10x.com>
2023-07-19 15:19:47 +0530

    I wrote code for integerTwo test in fibonacci class.Testcase is passed.

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -5,6 +5,10 @@ public class Fibonacci {
     public int findFibonacci(int n) {
         if (n<=0){
             return -1;
+        } else if (n==1) {
+            return 0;
+        } else if (n==2) {
+            return 1;
         }
         return n;
     }
--- src/fibonacci/FibonacciTest.java
+++ src/fibonacci/FibonacciTest.java
@@ -16,10 +16,22 @@ public class FibonacciTest {
         assertEquals(-1,negativeFibonacci);
         }
     @Test
-    public void givenAnyZero_returnNegativeOne() {
+    public void givenZero_returnNegativeOne() {
         Fibonacci fibonacci = new Fibonacci();
         int negativeFibonacci = fibonacci.findFibonacci(0);
         assertEquals(-1,negativeFibonacci);
+    }
+    @Test
+    public void givenOne_returnZero() {
+        Fibonacci fibonacci = new Fibonacci();
+        int firstFibonacci = fibonacci.findFibonacci(1);
+        assertEquals(0,firstFibonacci);
+    }
+    @Test
+    public void givenTwo_returnOne() {
+        Fibonacci fibonacci = new Fibonacci();
+        int secondFibonacci = fibonacci.findFibonacci(2);
+        assertEquals(1,secondFibonacci);
     }
     }
 
