4eac1da35a3c200f40217684de961c0c0e388ef6
SivaKumar <sivakumar.g@improve10x.com>
2023-07-25 16:36:09 +0530

    I wrote code for 10value Testcase. The Testcase is passed.

--- src/java/com/improve10x/tdd/templerun/SpikePit.java
+++ src/java/com/improve10x/tdd/templerun/SpikePit.java
@@ -5,13 +5,20 @@ public class SpikePit extends Obstacle{
 
     public SpikePit(int width) throws InvalidValueException {
         super("Spike Pit",0);
-        this.width = width;
-        throw new InvalidValueException();
+        if (width<0 || width>100){
+            throw new InvalidValueException();
 
+        }
+        this.width = width;
     }
     public void trap(Character character) {
         System.out.println(name + "trapped" + character.getName() + "inside ! . it is" + width + "meter wide");
         character.reduceHealth(damage);
+    }
+
+    public int getValue() {
+
+        return width;
     }
 
     public class InvalidValueException extends Exception{
--- src/test/templerun/SpikePitTest.java
+++ src/test/templerun/SpikePitTest.java
@@ -4,6 +4,7 @@ import com.improve10x.tdd.templerun.Fireball;
 import com.improve10x.tdd.templerun.SpikePit;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SpikePitTest {
@@ -15,5 +16,14 @@ public class SpikePitTest {
     public  void giveMinusOne_throwsInvalidException() {
         assertThrows(SpikePit.InvalidValueException.class,
                 () -> new SpikePit(-1),"SpikePit width should be between 0 to 100");
+    }
+    @Test
+    public  void give101_throwsInvalidException() {
+        assertThrows(SpikePit.InvalidValueException.class,
+                () -> new SpikePit(101),"SpikePit width should be between 0 to 100");
+    }
+    @Test
+    public void given10_WhenGetValueCalled_thenReturn10() throws SpikePit.InvalidValueException {
+        assertEquals(10,new SpikePit(10).getValue());
     }
 }
